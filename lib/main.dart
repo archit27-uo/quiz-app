f9645cc9afdfeee6eb2ede972d1b931139131f3d
archit27-uo <archit27chawda@gmail.com>
2023-04-19 17:31:47 +0530

    fifth

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,14 @@ void main() {
           title: const Text("Quizz"),
           backgroundColor: Colors.indigo,
         ),
-        body:const quizz(),
+        body: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+            child: quizz(),
+          ),
+
+        )
+
       ),
     ),
   );
@@ -91,127 +98,149 @@ class _quizzState extends State<quizz> {
   @override
   Widget build(BuildContext context) {
     return Column(
-     mainAxisAlignment: MainAxisAlignment.start,
+     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        const SizedBox(
-          height: 250,
-        ),
 
-        Text(
 
-          quizzBase.getQuestion(),
+        Expanded(
 
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 30,
-        ),
+          flex: 6,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+
+                quizzBase.getQuestion(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+
+                color: Colors.white,
+                fontSize: 30,
+              ),
+              ),
+            ),
+          ),
         ),
 
         // const SizedBox(
         //   height: 200,
         // ),
-        SizedBox(
-          height: 150,
-        ),
-        Row(
+        // SizedBox(
+        //   height: 150,
+        // ),
+        Expanded(
+          child: Row(
 
-          children: [
+            children: [
 
-            Expanded(
-              child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    foregroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(vertical: 28, horizontal: 10),
-                  ),
-                  onPressed: (){
-                    setState(() {
-                      scorePrinter(true);
-                      // if(quizzBase.getAnswer()){
-                      //   score.add(
-                      //     const Icon(
-                      //       Icons.check,
-                      //       color: Colors.green,
-                      //     ),
-                      //
-                      //   );
-                      // }
-                      // else{
-                      //
-                      //   score.add(
-                      //       const Icon(
-                      //         Icons.close,
-                      //         color: Colors.red,
-                      //       ),
-                      //   );
-                      // }
-                      quizzBase.questionIncrement();
-                    });
+              Expanded(
+                child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      foregroundColor: Colors.white,
+                     padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 10),
+                    ),
+                    onPressed: (){
+                      setState(() {
+                        scorePrinter(true);
+                        // if(quizzBase.getAnswer()){
+                        //   score.add(
+                        //     const Icon(
+                        //       Icons.check,
+                        //       color: Colors.green,
+                        //     ),
+                        //
+                        //   );
+                        // }
+                        // else{
+                        //
+                        //   score.add(
+                        //       const Icon(
+                        //         Icons.close,
+                        //         color: Colors.red,
+                        //       ),
+                        //   );
+                        // }
+                        quizzBase.questionIncrement();
+                      });
 
-                  },
-                  child: Text("TRUE")
+                    },
+                    child: const Text(
+                      "TRUE",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),)
+
+                ),
               ),
-            ),
 
-            Expanded(
-              child: TextButton(
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    foregroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(vertical: 28, horizontal: 10),
-                  ),
-                  onPressed: (){
+              Expanded(
+                child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 10),
+                    ),
+                    onPressed: (){
 
-                    setState(() {
-                      scorePrinter(false);
-                      // if(quizzBase.hasNextQus()){
-                      //   if(quizzBase.getAnswer()){
-                      //     score.add(
-                      //       const Icon(
-                      //         Icons.close,
-                      //         color: Colors.red,
-                      //       ),
-                      //     );
-                      //   }
-                      //   else{
-                      //
-                      //     score.add(
-                      //       const Icon(
-                      //         Icons.check,
-                      //         color: Colors.green,
-                      //       ),
-                      //     );
-                      //   }
-                      // }
-                      // else{
-                      //   Alert(
-                      //     context: context,
-                      //     type: AlertType.error,
-                      //     title: "Completed",
-                      //     desc: "All question has been answered",
-                      //     buttons: [
-                      //       DialogButton(
-                      //         child: Text(
-                      //           "Close",
-                      //           style: TextStyle(color: Colors.white, fontSize: 20),
-                      //         ),
-                      //         onPressed: () => Navigator.pop(context),
-                      //         width: 120,
-                      //       )
-                      //     ],
-                      //   ).show();
-                      //
-                      // }
-                      quizzBase.questionIncrement();
-                    });
-                  },
-                  child: Text("FALSE")
+                      setState(() {
+                        scorePrinter(false);
+                        // if(quizzBase.hasNextQus()){
+                        //   if(quizzBase.getAnswer()){
+                        //     score.add(
+                        //       const Icon(
+                        //         Icons.close,
+                        //         color: Colors.red,
+                        //       ),
+                        //     );
+                        //   }
+                        //   else{
+                        //
+                        //     score.add(
+                        //       const Icon(
+                        //         Icons.check,
+                        //         color: Colors.green,
+                        //       ),
+                        //     );
+                        //   }
+                        // }
+                        // else{
+                        //   Alert(
+                        //     context: context,
+                        //     type: AlertType.error,
+                        //     title: "Completed",
+                        //     desc: "All question has been answered",
+                        //     buttons: [
+                        //       DialogButton(
+                        //         child: Text(
+                        //           "Close",
+                        //           style: TextStyle(color: Colors.white, fontSize: 20),
+                        //         ),
+                        //         onPressed: () => Navigator.pop(context),
+                        //         width: 120,
+                        //       )
+                        //     ],
+                        //   ).show();
+                        //
+                        // }
+                        quizzBase.questionIncrement();
+                      });
+                    },
+                    child: Text(
+                      "FALSE",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ))
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-        Row(
-          children: score,
+        Expanded(
+          child: Row(
+            children: score,
+          ),
         )
       ],
     );
